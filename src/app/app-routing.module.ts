d287f29c3cbff59888d8933159d0ea64f574fb46
tritri <sakaitri@gmail.com>
2021-09-15 23:03:51 +0900

    loginページを最初に来るようルーティングを修正

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,13 @@ import { LoginComponent } from './login/login.component';
 import { GuardNameGuard } from './guard-name.guard';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/login-page', pathMatch: 'full' },
   { path: 'main', component: MainComponent, canActivate: [GuardNameGuard] },
   { path: 'login-page', component: LoginComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],//Webリロードについての対応 　<参考>　https://christina04.hatenablog.com/entry/2017/02/10/004253
+  imports: [RouterModule.forRoot(routes,{ useHash: true })],//Webリロードについての対応 　<参考>　https://christina04.hatenablog.com/entry/2017/02/10/004253
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
