71f49d16752274664300978effb1a288238ee523
tritri <sakaitri@gmail.com>
2021-10-17 18:10:14 +0900

    例とヘルプのCompoentを追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { LoginComponent } from './login/login.component';
 import { GuardNameGuard } from './guard-name.guard';
+import {ExplainComponent} from './explain/explain.component';
+import { ExampleComponent } from './example/example.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/main', pathMatch: 'full' },
   { path: 'main', component: MainComponent, canActivate: [GuardNameGuard] },
-  { path: 'login-page', component: LoginComponent }
+  { path: 'login-page', component: LoginComponent },
+  { path: 'explain', component: ExplainComponent },
+  { path: 'example', component: ExampleComponent },
 ];
 
 @NgModule({
